a90121e629173b92334624e24aa89c2b4f476d7f
David Simowitz <david.simowitz@gmail.com>
2018-09-17 17:02:05 -0400

    feat: create map

---
+++ js/map.js
@@ -0,0 +1,15 @@
+/**
+ * @file Implements and modifies map using Google Maps API
+ */
+
+var map;
+
+/**
+ * @description Initializes Map
+ */
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 40.690753, lng: -73.995638},
+    zoom: 15
+  });
+}
